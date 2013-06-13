8dee7c9684d9ecf282396deb062a017256416e0a
Keith Gapol <kgapol@sbcglobal.net>
2013-06-13 16:15:51 -0700

    Initial Postgresql Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8b666dae3b90c6987f1f89aab2871f04183c243e9d4e0950b365a73cb0f89299b67795eaf557cc2b94d8aa9ac34449ff8e9e428ccb8104e49357a9aedfb106df'

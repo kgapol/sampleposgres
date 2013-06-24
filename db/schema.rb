afdce890c6481de656c6de5dac9986bffb5f05ba
Keith Gapol <kgapol@sbcglobal.net>
2013-06-23 17:57:22 -0700

    Added password and encrypted_password attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130623065631) do
+ActiveRecord::Schema.define(:version => 20130624005323) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "encrypted_password"
   end
 
 end
